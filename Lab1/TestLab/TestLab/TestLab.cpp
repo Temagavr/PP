95b0fd94540fe972e618b6ba9eef2f28d685f3f4
Artem <artem-gavrilin-00@mail.ru>
2021-04-11 11:58:20 +0300

    Сданы ЛР1 и ЛР3

---
+++ Lab1/TestLab/TestLab/TestLab.cpp
@@ -0,0 +1,41 @@
+﻿#include <windows.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+
+DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
+{
+    //
+    // ?? тут выполняется полезная работа потока
+    //
+    std::stringstream temp;
+    temp << "Поток №" << (int)lpParam << " выполняется" << std::endl;
+    std::cout << temp.str();
+
+    ExitThread(0); // функция устанавливает код завершения потока в 0
+}
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "Russian");
+    int threadCount = std::stoi(argv[1]);
+    //std::cout << argv[1] << std::endl;
+    // создание двух потоков
+    HANDLE* handles = new HANDLE[threadCount];
+
+    int num = 0;
+    for (int i = 0; i < threadCount; ++i)
+    {
+        ++num;
+        handles[i] = CreateThread(NULL, 0, &ThreadProc, (LPVOID)num, CREATE_SUSPENDED, NULL);
+    }
+
+    for (int i = 0; i < threadCount; ++i)
+    {
+        ResumeThread(handles[i]);
+    }
+
+    // ожидание окончания работы двух потоков
+    WaitForMultipleObjects(threadCount, handles, true, INFINITE);
+    return 0;
+}
---
+++ Lab3/PPLab3/PPLab3/PPLab3.cpp
@@ -0,0 +1,48 @@
+﻿#include <windows.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <ctime>
+
+clock_t t;
+
+DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
+{
+    std::stringstream temp;
+    for (int i = 0; i < 20; ++i)
+    {
+        temp << (int)lpParam << " | " << (double)((clock() - t)) << " ; "; // /CLOCKS_PER_SEC можно добавить для вычисления секунд
+        std::cout << temp.str();
+        temp.str("");
+    }
+    ExitThread(0); // функция устанавливает код завершения потока в 0
+}
+
+int main(int argc, char* argv[])
+{
+
+    setlocale(LC_ALL, "Russian");
+    int threadCount = 2;
+    
+    // создание двух потоков
+    HANDLE* handles = new HANDLE[threadCount];
+
+    int num = 0;
+    for (int i = 0; i < threadCount; ++i)
+    {
+        ++num;
+        handles[i] = CreateThread(NULL, 0, &ThreadProc, (LPVOID)num, CREATE_SUSPENDED, NULL);
+    }
+
+    SetThreadAffinityMask(handles[0], 1);
+    SetThreadAffinityMask(handles[1], 1);
+
+    for (int i = 0; i < threadCount; ++i)
+    {
+        ResumeThread(handles[i]);
+    }
+
+    // ожидание окончания работы двух потоков
+    WaitForMultipleObjects(threadCount, handles, true, INFINITE);
+    return 0;
+}
